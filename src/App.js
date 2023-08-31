c33a3bc1bc002acf7aa56f767cbb9eebf762c6c0
Jitendra Kumar Bijarnia <70063345+jitendrakumar0@users.noreply.github.com>
2023-08-31 10:44:44 +0530

    jk

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import React, {useState} from 'react'
-import './App.scss';
-
+import React,{useEffect, useState} from 'react'
 function App() {
-  return (
+  const [count, setCount]=useState(0)
+  useEffect(()=>{
+    console.warn("UseEffect")
+  })
+  return(
     <div className="App">
-      <h2>Handle Form in React</h2>
-      <form onSubmit={getFormData}>
-        <input type="text" placeholder='enter name' onChange={(e)=>setName(e.target.value)} /><br /><br />
-        <select onChange={(e)=>setInterest(e.target.value)}>
-          <option>Select Options</option>
-          <option>Marvel</option>
-          <option>DC</option>
-        </select><br /><br />
-        <input type="checkbox" onChange={(e)=>setTnc(e.target.checked)} /> <span>Accept terms and conditions</span><br /><br />
-        <button type='submit'>Submit</button>
-        <button>Clear</button>
-      </form>
+      <h3>Hello World {count}</h3>
+      <button onClick={()=>{setCount(count+1)}}>Update Counter</button>
     </div>
-  );
+  )
 }
-
 export default App;
